a8094d31ef3996a29fc3588faba16b109069716f
Vidit Gupta <vidit135g@gmail.com>
2021-10-20 16:58:37 +0530

    Add files via upload

---
+++ sproutit/js/custom.js
@@ -0,0 +1,93 @@
+(function ($) {
+    'use strict';
+
+    $(document).on('ready', function () {
+        
+        // -----------------------------
+        //  Screenshot Slider
+        // -----------------------------
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+        
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+        
+        $('.speaker-slider').slick({
+            slidesToShow: 3,
+            centerMode: true,
+            infinite: true,
+            autoplay: true,
+            arrows:true,
+            responsive: [
+                {
+                    breakpoint: 1440,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 500,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
+         });
+        // -----------------------------
+        //  Count Down JS
+        // -----------------------------
+        $('.timer').syotimer({
+            year: 2021,
+            month: 9,
+            day: 21,
+            hour: 20,
+            minute: 30
+        });
+        // -----------------------------
+        // To Top Init
+        // -----------------------------
+        $('.to-top').on('click', function() {
+          $('html, body').animate({ scrollTop: 0 }, 'slow');
+          return false;
+        });
+        
+        // -----------------------------
+        // Magnific Popup
+        // -----------------------------
+        $('.image-popup').magnificPopup({
+            type: 'image',
+            removalDelay: 160, //delay removal by X to allow out-animation
+            callbacks: {
+                beforeOpen: function () {
+                    // just a hack that adds mfp-anim class to markup
+                    this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+                    this.st.mainClass = this.st.el.attr('data-effect');
+                }
+            },
+            closeOnContentClick: true,
+            midClick: true,
+            fixedContentPos: false,
+            fixedBgPos: true
+
+        });
+        // -----------------------------
+        // Mixitup
+        // -----------------------------
+        var containerEl = document.querySelector('.gallery-wrapper');
+        var mixer;
+        if (containerEl) {
+            mixer = mixitup(containerEl);
+        }
+    });
+
+})(jQuery);
